b5877548330751bcb648901e6c1aeeda9780c053
RFox <Rik-Fox>
2018-11-22 18:56:46 +0000

    Almost done

--- Assignment1_4.jl
+++ Assignment1_4.jl
@@ -5,7 +5,7 @@ pyplot()
 N=100
 μ = 2
 σ = 0.5
-samp = (randn(N).*σ) .+μ
+samp = pdf.(Normal(μ,σ),rand(N))
 
 histogram(samp,bins=100)
 xlabel!("X")
@@ -21,27 +21,25 @@ J = length(trial_σ)
 
 
 L = zeros(big_i,J)
-
-l = 1.0
+l= 1.0
 
 for i=1:big_i
       for j=1:J
+            l=1
             for k=1:length(samp)
-                  l = l*(1/sqrt(2*pi*trial_σ[j].^2))*exp(-((samp[k]-trial_μ[i])^2)/2*trial_σ[j].^2)
+                  l *= (1/sqrt(2*pi*trial_σ[j].^2))*exp(-((samp[k]-trial_μ[i])^2)/2*trial_σ[j].^2)
             end
 
             L[i,j]= l
-            l=1
       end
 end
 
 ind = findmax(L)
 
 heatmap(trial_μ,trial_σ,L)
-xticks!(trial_σ)
 
 
 
-predict_μ = trial_μ[ind[2][1]]
-predict_σ = trial_σ[ind[2][2]]
-trial_σ[ind[2][3]]
+# predict_μ = trial_μ[ind[2][1]]
+# predict_σ = trial_σ[ind[2][2]]
+# trial_σ[ind[2][3]]
--- Assignment1_5.jl
+++ Assignment1_5.jl
@@ -1,23 +1,20 @@
 using Distributions,SpecialFunctions,Statistics,StatsBase,DelimitedFiles,HypothesisTests,Plots
 pyplot()
 
-N = 5
-μ = 50
-σ2 = 9
+N = 50
+μ = 5
+σ2 = 7
 d = 0.1
 
 samp = rand(Normal(μ,sqrt(σ2)),N)
 samp_μ = mean(samp)
 samp_σ2 = var(samp)
 
-low = samp_μ - (1.96*sqrt(samp_σ2))
-high = samp_μ + (1.96*sqrt(samp_σ2))
-
-trial_μ = collect(low:d:high)
-trial_σ2 = collect(0.1:d:2*samp_σ2)
+trial_μ = collect(1:d:10)
+trial_σ2 = collect(1:d:10)
 trial_σ = sqrt.(trial_σ2)
 
-L = zeros(length(trial_σ2),length(trial_μ))
+L = zeros(length(trial_μ),length(trial_σ2))
 
 function pdfproduct(μ,σ,x)
     Π=1
@@ -29,13 +26,13 @@ end
 
 for i in 1:length(trial_μ)
     for j in 1:length(trial_σ2)
-        L[end+1-j,i] = pdfproduct(trial_μ[i],trial_σ[j],samp)
+        L[i,end+1-j] = pdfproduct(trial_μ[i],trial_σ[j],samp)
     end
 end
 
 # prior/length(prior)
 
-prior = ones(length(trial_σ2),length(trial_μ))
+prior = ones(length(trial_μ),length(trial_σ2))
 post = L .* prior
 P_data = sum(post/length(prior))
 
@@ -43,12 +40,14 @@ P = post/P_data
 
 heatmap(trial_μ,trial_σ2,P)
 
-#Using posterior as prior
-
-# prior2=posterior
-# posterior2=L .* prior2 /prob_of_data
-# heatmap(meanAxis,varAxis,posterior2)
+# μ_hat = (1/N)*sum(samp)
 #
-# prior3=posterior2
-# posterior3=L .* prior3 /prob_of_data
-# heatmap(meanAxis,varAxis,posterior3)
+# σ2_hat = (1/N)*sum((samp.-μ_hat).^2)
+#
+ ind = findmax(P)
+#
+# plot(P[:,ind[2][1]])
+# vline!([μ_hat])
+#
+# plot(P[ind[2][2],:])
+# vline!([σ2_hat])
